a0b24948426e3a0ca8380e3b32c7b176cb349001
Adil-Bub <adilahmed1090@gmail.com>
2020-07-11 21:58:30 +0530

    Updates

---
+++ precache-manifest.5ef14a33f3991d6bbce717ce1a2d4d6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16099f925791dc3fa2ee2dcc1ffcb607",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "d6f02ccfbf7e7e73d226",
+    "url": "/books/static/css/main.0a728893.chunk.css"
+  },
+  {
+    "revision": "929b29260471943d98f5",
+    "url": "/books/static/js/2.8248e3a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/books/static/js/2.8248e3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f02ccfbf7e7e73d226",
+    "url": "/books/static/js/main.82ee3f84.chunk.js"
+  },
+  {
+    "revision": "21a622327288988d064e",
+    "url": "/books/static/js/runtime-main.f2e6f98a.js"
+  },
+  {
+    "revision": "19c8757f2795e85d1c06336039433fe7",
+    "url": "/books/static/media/background.19c8757f.jpg"
+  }
+]);
